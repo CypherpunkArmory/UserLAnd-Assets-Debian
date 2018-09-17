e825a52b39877f4db97f563e51839d75f260cc76
Corbin Champion <corbinlc@gmail.com>
2018-09-17 11:28:34 -0700

    change default username to help with first round of testing

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [[ -z "${INITIAL_USERNAME}" ]]; then
-  INITIAL_USERNAME="user"
+  INITIAL_USERNAME="user2"
 fi
 if [[ -z "${INITIAL_PASSWORD}" ]]; then
   INITIAL_PASSWORD="userland"
