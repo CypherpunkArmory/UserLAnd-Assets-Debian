6e27543e3d92fc6f3828b86944c47d18e0902148
Corbin Champion <corbinlc@gmail.com>
2018-09-17 11:27:21 -0700

    add setting of default vnc password

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -7,6 +7,9 @@ if [[ -z "${INITIAL_PASSWORD}" ]]; then
   INITIAL_PASSWORD="userland"
 fi
 
-useradd $INITIAL_USERNAME -s /bin/bash -m -u 5000 
+useradd $INITIAL_USERNAME -s /bin/bash -m -u 4096 
 echo $INITIAL_USERNAME:$INITIAL_PASSWORD | chpasswd
 chsh -s /bin/bash $INITIAL_USERNAME
+
+mkdir -p /home/$INITIAL_USERNAME/.vnc/
+echo $INITIAL_PASSWORD | vncpasswd -f > /home/$INITIAL_USERNAME/.vnc/passwd
