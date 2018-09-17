df42d30749e4e27c221008247102bffcee3ab5a4
Corbin Champion <corbinlc@gmail.com>
2018-09-17 12:11:46 -0700

    only make user if it does not already exist

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
 if [[ -z "${INITIAL_USERNAME}" ]]; then
-  INITIAL_USERNAME="user2"
+  INITIAL_USERNAME="user"
 fi
 if [[ -z "${INITIAL_PASSWORD}" ]]; then
   INITIAL_PASSWORD="userland"
 fi
+if [[ -z "${INITIAL_VNC_PASSWORD}" ]]; then
+  INITIAL_VNC_PASSWORD="userland"
+fi
 
-useradd $INITIAL_USERNAME -s /bin/bash -m -u 4096 
-echo $INITIAL_USERNAME:$INITIAL_PASSWORD | chpasswd
-chsh -s /bin/bash $INITIAL_USERNAME
+if [ ! -d "/home/$INITIAL_USERNAME" ]; then
+  useradd $INITIAL_USERNAME -s /bin/bash -m -u 2000 
+  echo $INITIAL_USERNAME:$INITIAL_PASSWORD | chpasswd
+  chsh -s /bin/bash $INITIAL_USERNAME
 
-mkdir -p /home/$INITIAL_USERNAME/.vnc/
-echo $INITIAL_PASSWORD | vncpasswd -f > /home/$INITIAL_USERNAME/.vnc/passwd
+  mkdir -p /home/$INITIAL_USERNAME/.vnc/
+  echo $INITIAL_VNC_PASSWORD | vncpasswd -f > /home/$INITIAL_USERNAME/.vnc/passwd
+fi
