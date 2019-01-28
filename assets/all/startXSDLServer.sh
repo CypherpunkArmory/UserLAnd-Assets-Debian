53bd6c53dbae167b5ad49499a62bfc48fb83a315
Thomas Luong <Luongt@me.com>
2019-01-28 12:57:37 -0800

    Add one terminal upon start

--- assets/all/startXSDLServer.sh
+++ assets/all/startXSDLServer.sh
@@ -11,3 +11,11 @@ fi
 until env DISPLAY=:4721 xmessage -timeout 1 ignore-this ; do sleep 1 ; done
 
 nohup twm & echo $! > /tmp/xsdl.pidfile
+
+while [ ! -f /tmp/xsdl.pidfile ]
+do
+  sleep 1
+done
+
+xterm -geometry 80x24+0+0 -e /bin/bash --login &
+
