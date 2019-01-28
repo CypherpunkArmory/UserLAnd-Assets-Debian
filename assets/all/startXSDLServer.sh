3a3557b2c24156c0a1c8b969d4cef1f10c0eeb82
Thomas Luong <Luongt@me.com>
2019-01-28 15:16:19 -0800

    Add polling to wait until display environment variable is set

--- assets/all/startXSDLServer.sh
+++ assets/all/startXSDLServer.sh
@@ -8,5 +8,6 @@ if [[ -z "${PULSE_SERVER}" ]]; then
   PULSE_SERVER=localhost:4721
 fi
 
+until env DISPLAY=:4721 xmessage -timeout 1 ignore-this ; do sleep 1 ; done
 
 nohup twm & echo $! > /tmp/xsdl.pidfile
