cf924a8acff213ed6abd72ce58fcba2997fc6868
Thomas Luong <Luongt@me.com>
2019-01-28 12:28:00 -0800

    Remove quotes from Display variable

--- assets/all/startXSDLServer.sh
+++ assets/all/startXSDLServer.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
 if [[ -z "${DISPLAY}" ]]; then
-  DISPLAY=":4721"
+  DISPLAY=:4721
 fi
+
+if [[ -z "${PULSE_SERVER}" ]]; then
+  PULSE_SERVER=localhost:4721
+fi
+
 
 nohup twm & echo $! > /tmp/xsdl.pidfile
