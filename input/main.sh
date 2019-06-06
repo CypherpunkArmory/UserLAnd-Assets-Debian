ef38bb646705430f6c68da6b5856c702e7365989
Corbin Champion <corbinlc@gmail.com>
2019-06-06 12:39:35 -0700

    simplify and normalized the flow

--- input/main.sh
+++ input/main.sh
@@ -10,10 +10,18 @@ echo "unset LD_LIBRARY_PATH" >> /etc/profile.d/userland.sh
 echo "export LIBGL_ALWAYS_SOFTWARE=1" >> /etc/profile.d/userland.sh
 chmod +x /etc/profile.d/userland.sh
 
+#update our repos so we can install some packages
 echo "deb http://deb.debian.org/debian/ stable main contrib non-free" > /etc/apt/sources.list
 echo "#deb-src http://deb.debian.org/debian/ stable main contrib non-free" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian/ stable-updates main contrib non-free" >> /etc/apt/sources.list
 echo "#deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free" >> /etc/apt/sources.list
+apt-get update
+
+#install some packages with need for UserLAnd
+apt-get install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvncserver xterm xfonts-base twm expect
+
+#clean up after ourselves
+apt-get clean
 
 #tar up what we have before we grow it
 tar -czvf /output/rootfs.tar.gz --exclude sys --exclude dev --exclude proc --exclude mnt --exclude etc/mtab --exclude output --exclude input --exclude .dockerenv /
