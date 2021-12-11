fe9ae4deffed3daec6a7b0a7cabc3aeda945f641
Corbin Champion <corbinlc@gmail.com>
2021-12-11 11:30:50 -0800

    move to latest tag from dockerhub

--- input/main.sh
+++ input/main.sh
@@ -11,14 +11,10 @@ echo "export LIBGL_ALWAYS_SOFTWARE=1" >> /etc/profile.d/userland.sh
 chmod +x /etc/profile.d/userland.sh
 
 #update our repos so we can install some packages
-echo "deb http://deb.debian.org/debian/ buster main contrib non-free" > /etc/apt/sources.list
-echo "#deb-src http://deb.debian.org/debian/ buster main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://deb.debian.org/debian/ buster-updates main contrib non-free" >> /etc/apt/sources.list
-echo "#deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free" >> /etc/apt/sources.list
 apt-get update
 
 #install some packages with need for UserLAnd
-apt-get install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvncserver xterm xfonts-base twm expect
+apt-get install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvncserver xterm xfonts-base twm expect wget curl
 
 #clean up after ourselves
 apt-get clean
